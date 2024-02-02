d9f4e3f5a41db5dd8e98e51806989a8be10c8ec0
Jason Dorr <jasondorr@waterdogweb.com>
2024-02-02 09:33:45 -0500

    Completed number guessing game and starting on rock, paper, scissors game.

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,2 +1,26 @@
 import random
 
+secretNumber = random.randint(1, 20)
+print('Try to guess the number between 1 and 20.')
+print('You have 6 tries.')
+
+for guesses in range(1, 7):
+    guess = int(input())
+     
+    if guess > secretNumber:
+        print('Your guess is too high. Try again.')
+        
+    
+    elif guess < secretNumber:
+        print('Your guess is too low. Try again.')
+        
+    else:
+        break
+     
+if guess == secretNumber:
+    print(f'You win! The number was {secretNumber}.')
+    print(f'It took you {guesses} tries to get it correct.')
+    
+else:
+    print(f'You lose! The number was {secretNumber}')
+    
